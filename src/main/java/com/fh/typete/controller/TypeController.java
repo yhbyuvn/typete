009f97b150702bcc7b5ad00f29e5e19052271b4c
qjc <15036750423@163.com>
2021-01-14 21:27:26 +0800

    电商后台接口文档

--- src/main/java/com/fh/typete/controller/TypeController.java
+++ src/main/java/com/fh/typete/controller/TypeController.java
@@ -49,6 +49,16 @@ public class TypeController {
         ts.upType(ty);
         return RespData.success(null);
     }
+    /*
+    * 接口文档
+    *请求路径：http://localhost:8080/api/type/chaBrand
+    * 方式：post
+    * 参数：page(数据类型：Integer 当前页 必填项) limit（数据类型：Integer 每页条数 必填项） name（数据类型：String 名称）
+    *返回值：code 200 正常 400 数据错误 info：提示信息
+    *data :数据
+        取条数：dd.data.data.count
+        取全部数据：dd.data.data
+    * */
     @PostMapping("chaBrand")
     public RespData chaBrand(BrandVo bv){
         if (bv.getPage()==null){
@@ -60,6 +70,11 @@ public class TypeController {
         Map map=ts.chaBrand(bv);
         return RespData.success(map);
     }
+    /*
+     * 请求路径：http://localhost:8080/api/type/addBrand
+     * 参数：name（数据类型：String 名称 必填）
+     * 返回值 null
+     * */
     @PostMapping("addBrand")
     public RespData addBrand(Brand br){
         if (br.getName()==null){
@@ -71,6 +86,11 @@ public class TypeController {
         ts.addBrand(br);
         return RespData.success(null);
     }
+    /*
+     * 请求路径：http://localhost:8080/api/type/upBrand
+     * 参数：无
+     * 返回值 null
+     * */
     @PostMapping("upBrand")
     public RespData upBrand(Brand br){
         br.setAuthor("齐静春");
@@ -78,11 +98,28 @@ public class TypeController {
         ts.upBrand(br);
         return RespData.success(null);
     }
+    /*
+         * 请求路径：http://localhost:8080/api/type/upload
+         * 参数：imgpath(数据类型：String 图片名称 必填)
+         * 返回值  dd.data.filePath
+         * fileName: "-d4d92f16d3885cc.jpg",
+           filePath: "images/b1d2de62-261c-4a7c-b264-090cafa15666.jpg"
+         * */
     @PostMapping("upload")
     public RespData upload(MultipartFile imgpath, HttpServletRequest request){
         Map<String, String> images = Ud.ul(imgpath, request, "images");
         return RespData.success(images);
     }
+    /*
+   * 接口文档
+   *请求路径：http://localhost:8080/api/type/chaPro
+   * 方式：post
+   * 参数：page(数据类型：Integer 当前页 必填项) limit（数据类型：Integer 每页条数 必填项） name（数据类型：String 名称）
+   *返回值：code 200 正常 400 数据错误 info：提示信息
+   *data :数据
+       取条数：dd.data.data.count
+       取全部数据：dd.data.data
+   * */
     @PostMapping("chaPro")
     public RespData chaPro(ProVo pv){
         if (pv.getPage()==null){
@@ -94,6 +131,11 @@ public class TypeController {
         Map map=ts.chaPro(pv);
         return RespData.success(map);
     }
+    /*
+     * 请求路径：http://localhost:8080/api/type/addPro
+     * 参数：name（数据类型：String 名称 必填）
+     * 返回值 null
+     * */
     @PostMapping("addPro")
     public RespData addPro(Propertys pro){
         if (pro.getName()==null){
@@ -105,6 +147,11 @@ public class TypeController {
         ts.addPro(pro);
         return RespData.success(null);
     }
+    /*
+     * 请求路径：http://localhost:8080/api/type/upPro
+     * 参数：无
+     * 返回值 null
+     * */
     @PostMapping("upPro")
     public RespData upPro(Propertys pro){
         pro.setAuthor("齐静春");
